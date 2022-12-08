0b999f0287b61ceaa983f21266111fdcfb32f231
ciciDawgy <cmcdawgy@gmail.com>
2022-12-08 18:04:20 -0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridItems= document.querySelectorAll('.grid-item');
 const active = document.querySelector('.active');
 var modal = document.querySelector('.modal');
+const removeModal = document.querySelector('.fa-solid');
+
 
 /*gridItem.addEventListener('click', () => {
     modal.classlist.add('.active');
@@ -15,24 +17,91 @@ gridItems.forEach(gridItem => {
 
 gridItems.forEach(gridItem => {
     gridItem.addEventListener('click', () => {
+        removeActiveClasses()
         modal.classList.add('active');
     })
 })
+
+function removeActiveClasses() {
+    gridItems.forEach(gridItem => {
+        gridItem.classList.remove('active')
+    })
+}
+
+removeModal.addEventListener('click', () => {
+    modal.classList.remove('active');
+})
+
+const jeopardyCategories = [
+    {
+        genre: "WHO", 
+        questions: [
+                {
+                    question: "Why is the sky blue?", 
+                    answers: ["dunnu", "sup", "yo"],
+                    correct: "dunnu",
+                    level: "easy"
+                },
+                {
+                    question: "Why is grass green?", 
+                    answers: ["dunnu", "sup", "yo"],
+                    correct: "yo",
+                    level: "medium"
+                },
+                {
+                    question: "How many planets in the solar system?", 
+                    answers: ["dunnu", "sup", "yo"],
+                    correct: "yo",
+                    level: "hard"
+                },
+                {
+                    genre: "WHERE", 
+                    questions: [
+                {
+                    question: "Why is the sky blue?", 
+                    answers: ["dunnu", "sup", "yo"],
+                    correct: "dunnu",
+                    level: "easy"
+                },
+                {
+                    question: "Why is grass green?", 
+                    answers: ["dunnu", "sup", "yo"],
+                    correct: "yo",
+                    level: "medium"
+                },
+                {
+                    question: "How many planets in the solar system?", 
+                    answers: ["dunnu", "sup", "yo"],
+                    correct: "yo",
+                    level: "hard",
+                },
+            ]
+            },
+        ],
+    },
+]
+    
 
 // Get the modal
 
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+/*window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+} */
+/* 
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.className.remove = "active";
+    }
+  } */
 
-const btna = document.querySelector('.btna')
+/*const btna = document.querySelector('.btna')
 const input = document.querySelector('.input')
 
 btna.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
-}) 
+})  */
